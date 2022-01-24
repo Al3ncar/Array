14b0b71fc762c216b600a9013f83ac7a05855ec8
Al3ncar <alencar29htcss@gmail.com>
2022-01-24 19:39:34 -0300

    Array

--- script.js
+++ script.js
@@ -16,8 +16,5 @@ names.shift()
 console.log(names)
 //-------06-------//
 const numbers = [7,5,6,3,8,9,2,1,4]
-numbers.sort(function(a,b){
-    return a-b
-}
-)
+numbers.sort(function(a,b){return a-b})
 console.log(numbers)
